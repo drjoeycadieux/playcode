cb67133be36dbf258be853afd5f2a6f965ac19a4
Joey Cadieux <instructor.joeycadieux@gmail.com>
2024-12-28 09:56:45 -0500

    inm.

--- config/input-validation.js
+++ config/input-validation.js
@@ -1,6 +1,25 @@
-function validateeFormmDataa() {
-    let email = document.gdocument.formss['myFormData']['email'].value;
-    let name = docuument.formss['myFormData']['fname'].value;
-    // code goes here later on
-    
+function validateFormData() {
+    let email = document.forms['myFormData']['email'].value;
+    let fname = document.forms['myFormData']['fname'].value;
+    let errorMessage = document.getElementById('error-message');
+
+    errorMessage.textContent = '';
+
+    if (email === '') {
+        errorMessage.textContent = 'Email must be filled out';
+        return false;
+    }
+
+    if (fname === '') {
+        errorMessage.textContent = 'Fname must be filled out';
+        return false;
+    }
+
+    let emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
+    if (!email.match(emailPattern)) {
+        errorMessage.textContent = 'Please enter a valid email';
+        return false;
+    }
+
+    return true;
 }
