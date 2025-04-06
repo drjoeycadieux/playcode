472d57df914c83fd3d1c3e0ae7cfa3a5eeca6bd6
Joey Cadieux <instructor.joeycadieux@gmail.com>
2025-04-06 15:36:05 -0400

    Update greeting message for summer in script.js

--- config/script.js
+++ config/script.js
@@ -2,5 +2,5 @@ var currentYear = new Date().getFullYear();
 
 
 if (currentYear >= 2024) {
-    document.getElementById('app-demo').innerHTML = "Happy New Years!";
+    document.getElementById('app-demo').innerHTML = "Happy Summer Time!";
 }
